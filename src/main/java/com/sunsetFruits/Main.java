525296ed65c399fdfd1a9b242022c07bbabb3816
mt <manutm24@gmail.com>
2018-01-17 17:26:14 -0400

    Initial commit

---
+++ src/main/java/com/sunsetFruits/Main.java
@@ -0,0 +1,49 @@
+package com.sunsetFruits;
+
+
+import freemarker.template.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
+    public static void main(String[] args) throws SQLException {
+
+        port(4567);
+        enableDebugScreen();
+        logger.info("Iniciando aplicacion");
+
+        logger.info("Creando el folder estatico");
+        staticFileLocation("/public/");
+
+        logger.info("Crear el usuario admin por defecto");
+        //crearUsuarioAdmin();
+
+        logger.info("Creando la configuracion del template de freemarker");
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
+        configuration.setClassForTemplateLoading(Main.class, "/templates/");
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
+
+        get("/", (request, response) -> {
+            response.redirect("/home");
+            return null;
+        });
+
+        get("/home", (request, response) -> {
+           Map<String, Object> attributes = new HashMap<>();
+
+
+            return new ModelAndView(null, "/home.ftl");
+        }, freeMarkerEngine);
+    }
+}
